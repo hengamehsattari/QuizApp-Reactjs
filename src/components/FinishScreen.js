f9f4e972081a0ab4ef9f660b6a955af9ac9665f8
unknown <hengameh.sattari@yahoo.com>
2024-11-12 10:52:54 +0330

    reactjs-useReducer

---
+++ src/components/FinishScreen.js
@@ -0,0 +1,29 @@
+import React from "react";
+
+function FinishScreen({ points, maxPossiblePoints,highscore,dispatch }) {
+  const percentage = (points / maxPossiblePoints) * 100;
+  let emoji;
+  if (percentage === 100) emoji = "🥇";
+  if (percentage >= 80 && percentage < 100) emoji = "🎉";
+  if (percentage >= 50 && percentage < 80) emoji = "🙂";
+  if (percentage >= 0 && percentage < 50) emoji = "☹";
+  if (percentage === 0) emoji = "🤔";
+  return (
+    <>
+      <p className="result">
+        <span>{emoji}</span>You Scored <strong> {points} </strong> out of{" "}
+        {maxPossiblePoints}({Math.ceil(percentage)}%)
+      </p>
+      <p className="highscore">(Highscore: {highscore} points)</p>
+      <button
+        className="btn btn-ui"
+        onClick={() => dispatch({ type: "restart" })}
+      >
+        Restart Quiz
+      </button>
+
+    </>
+  );
+}
+
+export default FinishScreen;
